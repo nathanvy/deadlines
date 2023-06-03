f4422d6172ee81be22e8b02a0dac73dbe6b0efb9
Nathan Van Ymeren <nathan@polaris.local>
2023-06-02 20:28:14 -0700

    update timers to reflect finalized syllabus

--- init-timers.js
+++ init-timers.js
@@ -18,23 +18,23 @@ var timerlist = [
     }),
     new CountdownTimer({
 	selector: "#clock5",
-	tgtdate: new Date("2023-06-12T23:59:00.000-07:00")
+	tgtdate: new Date("2023-06-18T23:59:00.000-07:00")
     }),
     new CountdownTimer({
 	selector: "#clock6",
-	tgtdate: new Date("2023-06-26T23:59:00.000-07:00")
+	tgtdate: new Date("2023-07-02T23:59:00.000-07:00")
     }),
     new CountdownTimer({
 	selector: "#clock7",
-	tgtdate: new Date("2023-07-16T23:59:00.000-07:00")
+	tgtdate: new Date("2023-07-22T23:59:00.000-07:00")
     }),
     new CountdownTimer({
 	selector: "#clock8",
-	tgtdate: new Date("2023-07-17T23:59:00.000-07:00")
+	tgtdate: new Date("2023-07-23T23:59:00.000-07:00")
     }),
     new CountdownTimer({
 	selector: "#clock9",
-	tgtdate: new Date("2023-07-30T12:00:00.000-07:00")
+	tgtdate: new Date("2023-07-30T10:00:00.000-07:00")
     })
 ];
 
